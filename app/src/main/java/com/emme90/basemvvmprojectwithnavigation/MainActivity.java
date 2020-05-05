32ed51eeeeecc82fa47f79fd838e2b5a37178bd0
milos <milos.ribera@gmail.com>
2020-05-05 18:11:02 +0200

    Initial commit

---
+++ app/src/main/java/com/emme90/basemvvmprojectwithnavigation/MainActivity.java
@@ -0,0 +1,14 @@
+package com.emme90.basemvvmprojectwithnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
